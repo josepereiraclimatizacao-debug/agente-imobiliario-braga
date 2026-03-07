09f4f0a9bdaecd0d89b25dd3e351303cac537daa
josepereiraclimatizacao-debug <josepereiraclimatizacao@gmail.com>
2026-03-07 20:47:52 +0100

    Fix selector for anuncios in agent.py

--- agent.py
+++ agent.py
@@ -72,7 +72,7 @@ for URL in URLS:
 
     soup = BeautifulSoup(response.text, "html.parser")
 
-    anuncios = soup.select("article, .item, .offer-item, .listing-
+anuncios = soup.select("article, .item, .offer-item, .listing-item")
 
  for anuncio in anuncios[:30]:
 
